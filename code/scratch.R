740767c369c77ae983bade361f17adca3aadbce3
bcarancibia <bcarancibia@gmail.com>
2016-05-07 00:44:32 -0400

    update

---
+++ code/scratch.R
@@ -0,0 +1,6 @@
+
+season15 <- read.csv("/home/bcarancibia/nba_analysis/data/1516_playerstats.csv", header=TRUE, sep=",")
+season14 <- read.csv("/home/bcarancibia/nba_analysis/data/1415_playerstats.csv", header=TRUE, sep=",")
+season13 <- read.csv("/home/bcarancibia/nba_analysis/data/1314_playerstats.csv", header=TRUE, sep=",")
+
+
